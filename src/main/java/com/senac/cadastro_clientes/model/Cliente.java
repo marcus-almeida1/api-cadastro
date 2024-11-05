edd2cca091702db1e78d7eafd8c018ab8d650288
marcus-almeida1 <marcusvinicius20.almeida@gmail.com>
2024-11-05 13:31:30 -0300

    feat: criado as entidades

---
+++ src/main/java/com/senac/cadastro_clientes/model/Cliente.java
@@ -0,0 +1,106 @@
+package com.senac.cadastro_clientes.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String sobrenome;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column
+    private String sexo;
+    @Column(name = "dt_nascimento")
+    private LocalDate dataNascimento;
+    @OneToMany(mappedBy = "cliente")
+    private List<Endereco> enderecos;
+    @Column(name = "dt_cadastro")
+    private LocalDateTime dataCadastro;
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, String sobrenome, String email, String sexo, LocalDate dataNascimento, List<Endereco> enderecos, LocalDateTime dataCadastro) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.enderecos = enderecos;
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
---
+++ src/main/java/com/senac/cadastro_clientes/model/Endereco.java
@@ -0,0 +1,95 @@
+package com.senac.cadastro_clientes.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String rua;
+    @Column(nullable = false)
+    private String bairro;
+    @Column(nullable = false)
+    private String cidade;
+    @Column(nullable = false)
+    private String uf;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+    @Column(name = "dt_cadastro")
+    private LocalDateTime dataCadastro;
+
+    public Endereco() {
+    }
+
+    public Endereco(Long id, String rua, String bairro, String cidade, String uf, Cliente cliente, LocalDateTime dataCadastro) {
+        this.id = id;
+        this.rua = rua;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.cliente = cliente;
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
