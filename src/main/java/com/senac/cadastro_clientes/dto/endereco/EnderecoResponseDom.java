fe54ce331e6df09383f31fd26e1187f45076bd4f
MarcusVVA <marcusvinicius20.almeida@gmail.com>
2024-11-09 19:25:46 -0300

    feat: criado os DTOs

---
+++ src/main/java/com/senac/cadastro_clientes/dto/endereco/EnderecoResponseDom.java
@@ -0,0 +1,72 @@
+package com.senac.cadastro_clientes.dto.endereco;
+
+import com.senac.cadastro_clientes.model.Cliente;
+
+import java.time.LocalDateTime;
+
+public class EnderecoResponseDom {
+
+    private Long id;
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private Cliente cliente;
+    private LocalDateTime dataCadastro;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
